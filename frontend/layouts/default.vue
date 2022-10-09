85c26d55b6aacbd41e30b9554aad758819f40bf0
yoshihiroshu <yoshihiro.shu@zuuonline.com>
2022-10-10 03:49:57 +0900

    add default.vuec

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <NavBar />
+        <Header />
+        <TopicNav />
+        <NuxtPage />
+        <div class="container mx-auto flex flex-wrap py-6">
+            <Article />
+            <Sidebar />
+        </div>
+        <footer />
+    </div>
+</template>
+
+<script setup>
+</script>
